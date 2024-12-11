a6e2e3c82ea4c7011c7f7b94b93ca75e4183a632
Liza-b-13 <153221867+Liza-b-13@users.noreply.github.com>
2024-12-11 22:49:07 +0100

    Update TimeSeriesDataset.cpp

--- TP2/TimeSeriesDataset.cpp
+++ TP2/TimeSeriesDataset.cpp
@@ -11,3 +11,19 @@ TimeSeriesDataset::TimeSeriesDataset()
 // Constructeur paramétré
 TimeSeriesDataset::TimeSeriesDataset(bool znormalize, bool isTrain, int maxLength, int numberOfSamples)
     : znormalize(znormalize), isTrain(isTrain), maxLength(maxLength), numberOfSamples(numberOfSamples) {}
+
+
+// Function to calculate Euclidean distance between two time series
+double TimeSeriesDataset::euclidean_distance(const vector<double>& series1, const vector<double>& series2) {
+    if (series1.size() != series2.size()) {
+        throw invalid_argument("The two series must have the same length.");
+    }
+
+    double distance = 0.0;
+    for (size_t i = 0; i < series1.size(); ++i) {
+        double diff = series1[i] - series2[i];
+        distance += diff * diff;
+    }
+
+    return sqrt(distance);
+}
